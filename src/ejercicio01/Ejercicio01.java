ab088a440be3b947d7b8a44bdd31abe673102782
DAM <DAM@DAM-12>
2024-01-17 10:27:06 +0100

    Primer registro de todo

---
+++ src/ejercicio01/Ejercicio01.java
@@ -0,0 +1,43 @@
+package ejercicio01;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	/*
+	 * Hacer un programa que pida al usuario que introduzca
+	 * una frase por pantalla, y muestre por pantalla cuántas
+	 * vocales hay en el texto
+	*/
+	public static boolean esVocal(char car) {
+		//Implementamos diferentes lógicas para comprobar si es vocal
+		boolean es_vocal = false;
+		
+		return es_vocal;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String cadena = "";
+		int numeroVocales = 0;
+		
+		
+		System.out.println("Por favor, introduce la cadena de texto: ");
+		cadena = sc.nextLine();
+		for(int i = 0; i<cadena.length(); i++) {
+			if(esVocal(cadena.charAt(i)));
+				numeroVocales++;
+			
+			
+		}
+		
+		System.out.println("El numero de vocales es: " + numeroVocales);
+		
+		
+		
+		sc.close();
+		System.out.println("Bye");
+	}
+
+}
